82bc44f27b47c64e4fe402b5c7b1f5ed9252f2bc
Tomasitok <tomasaristizabalgomez@gmail.com>
2023-05-22 09:54:18 -0500

    siu

---
+++ 1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *_strdup(char *str)
+{
+    if (str == NULL)
+    {
+        return NULL;
+    }
+
+    int length = strlen(str);
+
+    char *duplicate = (char *)malloc((length + 1) * sizeof(char));
+    if (duplicate == NULL)
+    {
+        return NULL;
+    }
+
+    strcpy(duplicate, str);
+
+    return duplicate;
+}
